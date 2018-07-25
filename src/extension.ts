c1b5f04c09a0e16a9c529fd0a43ee27aee84288b
Mathew Robinson <chasinglogic@gmail.com>
2018-07-25 12:50:22 -0400

    Periodically update the status bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,45 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { window, commands, ExtensionContext, StatusBarAlignment } from 'vscode';
 import { workingOn, completeTask, newTaskInput, incompleteTasks } from './tsk';
 import { commandWithInput, commandFrom, commandWithQuickPick } from './utils';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
+    const status = window
+        .createStatusBarItem(StatusBarAlignment.Right, 100);
+    status.command = 'extension.currentTask';
+    context.subscriptions.push(status);
+
+    updateStatus(status);
+    // Update status bar every 30 seconds
+    setInterval(() => { updateStatus(status); }, 30 * 1000);
+
     context.subscriptions.push(
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             'extension.newTask', commandWithInput(newTaskInput)));
 
     context.subscriptions.push(
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             'extension.completeTask', commandFrom(completeTask)));
 
-    context.subscriptions.push(vscode.commands.registerCommand(
+    context.subscriptions.push(commands.registerCommand(
         'extension.completeTaskById', commandWithQuickPick((selected) => {
             completeTask(selected.split(' ')[0]);
         }, incompleteTasks)
     ));
+}
 
-    const status = vscode.window
-        .createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-    status.command = 'extension.currentTask';
-    status.text = 'Working On: Loading...';
-    status.show();
-    context.subscriptions.push(status);
-
+function updateStatus(status: StatusBarItem) {
     workingOn().then((taskTitle: string) => {
-        status.text = `Working On: ${taskTitle}`;
+        if (taskTitle) {
+            status.text = `Working On: ${taskTitle}`;
+            status.show();
+        } else {
+            status.hide();
+        }
     });
 }
 
